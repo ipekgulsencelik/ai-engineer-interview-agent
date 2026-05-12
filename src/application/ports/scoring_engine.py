84ef90218a1fee1490416475a2b2e02043d66681
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2026-05-12 20:27:56 +0300

    Refactor scoring engine and add policy tests

--- src/application/ports/scoring_engine.py
+++ src/application/ports/scoring_engine.py
@@ -4,12 +4,13 @@ from abc import ABC
 from abc import abstractmethod
 
 from src.domain.entities.question import Question
-from src.domain.scoring.scoring_breakdown import (
-    ScoringBreakdown,
+from src.domain.results.selection_breakdown import (
+    SelectionBreakdown,
 )
 from src.domain.scoring.scoring_context import (
     ScoringContext,
 )
+
 
 class ScoringEngine(ABC):
     """
@@ -22,7 +23,7 @@ class ScoringEngine(ABC):
         *,
         question: Question,
         context: ScoringContext,
-    ) -> ScoringBreakdown:
+    ) -> SelectionBreakdown:
         """
         Verilen question için explainable scoring sonucu üretir.
         """
